3aa11101b15bdacf0d38893253a401d1497125f8
Jenna <jijung0531@gmail.com>
2023-02-08 18:47:51 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Jenna";
+  const list = ["우유","딸기","바나나"]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <h1 className="orange">{`Hello! ${name}`}</h1>
+  <h1 className="orange">Hello! {name}</h1>
+  <h2>Hello!</h2>
+  <p>{name}</p>
+  <ul>
+    <li>우유</li>
+    <li>딸기</li>
+    <li>바나나</li>
+  </ul>
+  <ul>
+    {list.map((item) =>(
+      <li>{item}</li>
+    ))}
+  </ul>
+  <img 
+  style={{ width: '200px', height: '200px'}}
+  src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fc6%2F2e%2Fa5%2Fc62ea5db353fc2b75bed7fb3be42c251.jpg&type=sc960_832" 
+  alt="nature" />
+  </>
+  )
 }
 
 export default App;
